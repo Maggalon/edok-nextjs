999283da5e1b8430a7c3434b186462bf6c0d4ea7
Maggalon <abstergo22h@gmail.com>
2025-03-04 15:37:14 +1000

    fixing

--- app/api/mapbox/route.ts
+++ app/api/mapbox/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default function handler(req: NextRequest, res: NextResponse) {
+export default function GET(req: NextRequest, res: NextResponse) {
     return NextResponse.json({ token: process.env.MAPBOX_TOKEN, status: 200 })
 }
